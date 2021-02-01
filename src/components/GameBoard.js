89ec46b212f52cc71072c065e7e93a7f572840d2
Nina Jivetorp <ninajivetorp@gmail.com>
2021-02-01 17:13:07 +0100

    moved fetch to own component

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import MemoryCard from "./MemoryCard";
+import FetchImages from "./FetchImages";
 
 function GameBoard(props) {
   const [images, setImages] = useState([]);
@@ -7,50 +8,6 @@ function GameBoard(props) {
   const [secondCard, setSecondCard] = useState(0);
   const [isClicked, setIsClicked] = useState([]);
   const [lockGame, setLockGame] = useState(false);
-
-  useEffect(() => {
-    async function getImages() {
-      const key = "e97530c4db33a4ae21d65f765fe9c551";
-      const response = await fetch(
-        `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=animal&format=json&nojsoncallback=1&page=2&page=1`
-      );
-      const data = await response.json();
-
-      const fetchedImages = [];
-      for (let i = 0; i < 8; i++) {
-        fetchedImages.push(getImageUrl(data.photos.photo[i]));
-        fetchedImages.push(getImageUrl(data.photos.photo[i]));
-      }
-
-      setImages(shuffle(fetchedImages));
-    }
-
-    getImages();
-  }, []);
-
-  function getImageUrl(photo) {
-    let size = "q";
-    let imgUrl = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
-
-    return imgUrl;
-  }
-
-  function shuffle(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
 
   useEffect(() => {
     compareCards();
@@ -82,6 +39,7 @@ function GameBoard(props) {
 
   return (
     <div className="game-board">
+      <FetchImages setImages={setImages} />
       {images.map((image, index) => {
         return (
           <MemoryCard
